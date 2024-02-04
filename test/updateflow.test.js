5e39d96a363ee34bfd4797ce9200824cacfa4831
BigThunderSR <17056173+BigThunderSR@users.noreply.github.com>
2024-02-04 16:45:08 -0600

    Cleanup test file

--- test/updateflow.test.js
+++ test/updateflow.test.js
@@ -1,8 +1,6 @@
 test('Running should update flow', async () => {
   const spawnSync = require('child_process').spawnSync;
   const myResult = spawnSync('npm run start', { stdio: "pipe", shell: true });
-  const logSpy = jest.spyOn(global.console, 'log');    
-  //expect(myResult.stdout.toString()).toBe('Module already at latest version');
   console.log(myResult.output.toString());
   expect(myResult.output.toString().includes('Module already at latest version')).toBeTruthy();
 });
