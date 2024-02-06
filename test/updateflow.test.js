156086dc5d13bd11c93986ff539a97a43b39175e
BigThunderSR <17056173+BigThunderSR@users.noreply.github.com>
2024-02-06 11:06:33 -0600

    Updated test cases

--- test/updateflow.test.js
+++ test/updateflow.test.js
@@ -1,6 +1,14 @@
+test('Running should fail since package name not provided', async () => {
+  const spawnSync = require('child_process').spawnSync;  
+  const myResult = spawnSync('npm run start', { stdio: "pipe", shell: true });
+  console.log(myResult.output.toString());
+  expect(myResult.output.toString().includes('No package name provided')).toBeTruthy();
+});
+
 test('Running should update flow', async () => {
   const spawnSync = require('child_process').spawnSync;
-  const myResult = spawnSync('npm run start', { stdio: "pipe", shell: true });
+  const envVars = Object.assign({}, process.env, { INPUT_PACKAGE_NAME: 'node-red-contrib-onstar2' });
+  const myResult = spawnSync('npm run start', { stdio: "pipe", shell: true, env: envVars });
   console.log(myResult.output.toString());
   expect(myResult.output.toString().includes('Module already at latest version')).toBeTruthy();
 });
